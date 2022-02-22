35280754333b27cf1ea1c89ed82f90c10f4bf5d5
Oleg Kravchenko <kitaist@kitaist.com>
2022-02-23 05:53:45 +0800

    Lesson 19, initial commit

---
+++ dist/greeting/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const title = document.querySelector('h1')
+const weekday = document.getElementById('weekday')
+const currentTime = document.getElementById('current-time')
+const newYearDays = document.getElementById('new-year')
+
+// Добрый день (утро, вечер, ночь в зависимости от времени суток)
+
+// Сегодня: Понедельник
+
+// Текущее время: 12:05:15 PM
+
+// До нового года осталось 175 дней
+
+console.log(new Date())
