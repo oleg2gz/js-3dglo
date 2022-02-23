f9f1e959f380f24ceb614e70d3b299fb16b122f6
Oleg Kravchenko <kitaist@kitaist.com>
2022-02-23 10:14:20 +0800

    Lesson 19, part 2 ready

--- dist/greeting/script.js
+++ dist/greeting/script.js
@@ -1,16 +1,50 @@
 'use strict'
 
 const title = document.querySelector('h1')
-const weekday = document.getElementById('weekday')
-const currentTime = document.getElementById('current-time')
-const newYearDays = document.getElementById('new-year')
+const spanWeekday = document.getElementById('weekday')
+const spanCurrentTime = document.getElementById('current-time')
+const spanNewYearDays = document.getElementById('new-year')
 
-// Добрый день (утро, вечер, ночь в зависимости от времени суток)
+const date = new Date()
+const hour = date.getHours()
+const weekday = date.toLocaleString('ru-Ru', { weekday: 'long' })
+const msNewYear = new Date(2023, 0, 1).getTime()
+const msOneDay = 1000 * 60 * 60 * 24
+const daysToNewYear = Math.round((msNewYear - date.getTime()) / msOneDay)
 
-// Сегодня: Понедельник
+// Склонение числительных
+const titles = ['день', 'дня', 'дней']
+const decOfNum = (number, titles) => {
+  const cases = [2, 0, 1, 1, 1, 2]
 
-// Текущее время: 12:05:15 PM
+  return titles[
+    number % 100 > 4 && number % 100 < 20
+      ? 2
+      : cases[number % 10 < 5 ? number % 10 : 5]
+  ]
+}
 
-// До нового года осталось 175 дней
+switch (true) {
+  case hour >= 4 && hour < 11:
+    title.textContent = 'Доброе утро!'
+    break
 
-console.log(new Date())
+  case hour >= 11 && hour < 18:
+    title.textContent = 'Добрый день!'
+    break
+
+  case hour >= 18 && hour < 23:
+  case hour >= 0 && hour < 4:
+    title.textContent = 'Доброй ночи!'
+}
+
+spanWeekday.textContent = weekday[0].toUpperCase() + weekday.slice(1)
+spanCurrentTime.textContent = date.toLocaleString('en-En', {
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+})
+spanNewYearDays.textContent = `${daysToNewYear} ${decOfNum(
+  daysToNewYear,
+  titles
+)}`
