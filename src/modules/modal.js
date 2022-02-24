6b013a9819104499d5dadc74a118b19c783063c8
Oleg Kravchenko <kitaist@kitaist.com>
2022-02-24 08:53:48 +0800

    Lesson 20, modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,18 +1,54 @@
 const modal = () => {
   const modal = document.querySelector('.popup')
+  const modalContent = modal.querySelector('.popup-content')
   const btnClose = modal.querySelector('.popup-close')
   const btns = document.querySelectorAll('.popup-btn')
+  let idAnimationOpen
+  let idAnimationClose
+  let counter = 0
 
-  // Написать анимацию появления модального окна
-  // Если пользователь заходит на сайт с устройства с шириной экрана меньше 768px (мобильного устройства) - анимация отключается
+  const openAnimation = () => {
+    counter += 0.05
+    idAnimationOpen = requestAnimationFrame(openAnimation)
+    if (counter < 1) {
+      modal.style.opacity = counter
+      modalContent.style.opacity = counter
+    } else {
+      modal.style.opacity = 1
+      modalContent.style.opacity = 1
+      cancelAnimationFrame(idAnimationOpen)
+    }
+  }
+
+  const closeAnimation = () => {
+    counter -= 0.05
+    idAnimationClose = requestAnimationFrame(closeAnimation)
+    if (counter > 0) {
+      modal.style.opacity = counter
+      modalContent.style.opacity = counter
+    } else {
+      modal.style.display = 'none'
+      modalContent.style.opacity = 0
+      cancelAnimationFrame(idAnimationClose)
+    }
+  }
 
   btns.forEach((btn) => {
     btn.addEventListener('click', () => {
-      modal.style.display = 'block'
+      if (window.innerWidth >= 768) {
+        modal.style.display = 'block'
+        openAnimation()
+      } else {
+        modal.style.display = 'block'
+      }
     })
   })
   btnClose.addEventListener('click', () => {
-    modal.style.display = 'none'
+    if (window.innerWidth >= 768) {
+      closeAnimation()
+    } else {
+      modal.style.display = 'none'
+    }
   })
 }
 
